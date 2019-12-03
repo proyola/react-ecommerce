5e4f7a331a59c47860654063a09aeb2d628154e7
paloma <pramirezo@minsait.com>
2019-12-03 17:08:46 +0100

    routing

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -4,7 +4,7 @@ import './preview-collection.styles.scss'
 
 const CollectionPreview = ({title, items}) => (
   <div className='collection-preview'>
-    <h1 className='title'> {title.toUperCase()}</h1>
+    <h1 className='title'> {title.toUpperCase()}</h1>
     <div className='preview'>
       { 
         items
